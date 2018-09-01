0eab73e2f80ecc45dc2e6a6a03bb461e0f221ea4
Sergej Alikov <sergej.alikov@gmail.com>
2018-09-02 00:37:14 +0100

    Properly backup and restore shell options

--- shute.sh
+++ shute.sh
@@ -77,17 +77,27 @@ shute_run_test_case () {
     {
         TIMEFORMAT=%R
 
+        # Backup shell options. errexit is propagated to process
+        # substitutions, therefore no special handling is needed
+
+        declare -a SHUTE_SHELL_OPTIONS
+        readarray -t SHUTE_SHELL_OPTIONS < <(shopt -po)
+
         set +e
 
         # sequence numbers added by the last component allow
         # user to perform sorting (sort -V) to split STDOUT and STDERR into
         # separate blocks (preserving the correct order within the block)
         # and reassemble back into a single block later (sort -V -k 2).
-        time eval '(set -eu; unset TIMEFORMAT; eval "$1" 2> >(sed -u -e "s/^/STDERR /") > >(sed -u -e "s/^/STDOUT /"))' > >(grep -n '.' | sed -u 's/^\([0-9]\+\):\(STDOUT\|STDERR\) /\2 \1 /')
+        time eval '(set -eu; unset TIMEFORMAT SHUTE_SHELL_OPTIONS; eval "$1" 2> >(sed -u -e "s/^/STDERR /") > >(sed -u -e "s/^/STDOUT /"))' > >(grep -n '.' | sed -u 's/^\([0-9]\+\):\(STDOUT\|STDERR\) /\2 \1 /')
 
         declare rc="$?"
 
-        set -e
+        # Restore shell options
+        declare cmd
+        for cmd in "${SHUTE_SHELL_OPTIONS[@]}"; do
+            eval "$cmd"
+        done
 
     } 2> >(sed -u -e "s/^/TIME /")
 
